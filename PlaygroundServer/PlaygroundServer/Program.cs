ace3dfa27e0e3eeeaabc0338e6f097b6279401e5
Christian Sejer <christian_sejer@hotmail.com>
2020-10-27 17:12:01 +0100

    Initial commit

---
+++ PlaygroundServer/PlaygroundServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlaygroundServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

92da8aca5d2337f3c404d09d18d9acabe12d8f49
Christian Sejer <christian_sejer@hotmail.com>
2020-10-27 20:05:55 +0100

    Added communication functionality

---
+++ PlaygroundServer/PlaygroundServer/ServerHandle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaygroundServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int fromClient, Packet packet)
+        {
+            int clientIDCheck = packet.ReadInt();
+            string username = packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {fromClient}");
+            if(fromClient != clientIDCheck)
+            {
+                Console.WriteLine($"Player \"{username}\" (ID: {fromClient}) has assumed the wrong client ID ({clientIDCheck})");
+            }
+        }
+    }
+}
